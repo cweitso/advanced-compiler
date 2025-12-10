80471fd33ed4f37255744a13c2a0964b5e235566
dannytso <dannytuzoo@gmail.com>
2025-12-10 11:28:21 +0800

    Remove hw1 files for hw2 branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# 作業官方說明
-- https://hackmd.io/@SBJq1D9-RV-kEX6m3-nHLA/r1502-TTxe
--- hw1/test1.c
+++
@@ -1,9 +0,0 @@
-int main(){
-    int i;
-    int A[20], B[20], C[20];
-    for (i = 4; i < 20; i++) {
-        A[i] = C[i];      // Statement 1 (S1)
-        B[i] = A[i - 4];  // Statement 2 (S2)
-    }
-    return 0;
-}
